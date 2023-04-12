a3c260b609113f0f202d899f724cb05fed3a5c2a
DESKTOP-UAKANTS\DanilErem <danilerem@gmail.com>
2023-04-12 19:34:56 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ bot.on("message", async (ctx) => {
       parts.splice(0, 1);
       const id = parts.join(" ");
       if (user) {
-        await ctx.unbanChatSenderChat(id);
+        await bot.telegram.unbanChatMember(id);
         await ctx.reply(
           `Пользователь с ником "${user.name}" раззабанен по айди ${user.id}!`
         );
@@ -74,7 +74,7 @@ bot.on("message", async (ctx) => {
       console.log("!Iportant!", ctx.message.reply_to_message);
 
       if (ctx.message.text && ctx.message.text == Command.Ban) {
-        await ctx.banChatSenderChat(id);
+        await bot.telegram.banChatMember(id);
         await ctx.reply(
           `Пользователь с ником "${user.name}" забанен по айди ${user.id}!`
         );
